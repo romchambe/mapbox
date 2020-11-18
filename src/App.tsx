f23cb93bf5a32e49484cd5c9fca95ffd84f8ceba
romchambe <chamberomain@gmail.com>
2020-11-18 10:21:06 +0100

    remove useless state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 
 export function App() {
-const [lng, setLng] = useState(5) 
-const [lat, setLat] = useState(34) 
-const [zoom, setZoom] = useState(2) 
+
 const setMap = useCallback(container => {
   new mapboxgl.Map({
     container,
     style: 'mapbox://styles/mapbox/streets-v11',
-    center: [lng, lat],
-    zoom: zoom
+    center: [5, 32],
+    zoom: 2
   })
 }, [])
 
