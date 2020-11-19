0a8dec20c7974dc328e798c774aeaea21d626ca7
romchambe <chamberomain@gmail.com>
2020-11-19 12:22:46 +0100

    implement layer as a class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,60 @@
-import React, { useCallback } from "react"
-import mapboxgl from "mapbox-gl"
+import React, { useCallback, useEffect, useState } from "react"
+import mapboxgl, { MapboxEvent } from "mapbox-gl"
 import "../assets/styles/tailwind.output.css"
+import { centerLat, centerLng, Layer } from "../map/createLayer"
 
 export function App(): JSX.Element {
-  const setMap = useCallback((container) => {
-    new mapboxgl.Map({
-      container,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [5, 32],
-      zoom: 2,
-    })
-  }, [])
+  const [mapEl, setMapEl] = useState<null | mapboxgl.Map>(null)
+
+  const setMap = useCallback(
+    (container) => {
+      if (container && !mapEl) {
+        const map = new mapboxgl.Map({
+          container,
+          style: "mapbox://styles/mapbox/streets-v11",
+          zoom: 14,
+          center: [centerLng, centerLat],
+          pitch: 60,
+          antialias: true,
+        })
+
+        setMapEl(map)
+      }
+    },
+    [mapEl]
+  )
+
+  useEffect(() => {
+    if (mapEl) {
+      mapEl.on("load", addLayer)
+
+      return () => {
+        mapEl.off("load", addLayer)
+      }
+    }
+  }, [mapEl])
 
   return (
     <div className="p-8 pb-12 h-screen">
       <div className="h-full" ref={setMap} />
     </div>
   )
+}
+
+// function moveListener(ev: MapboxEvent): void {
+//   console.log(
+//     "EVENT",
+//     ev.target.getCenter().lat,
+//     ev.target.getCenter().lng
+//   )
+// }
+
+function addLayer(ev: MapboxEvent): void {
+  console.log("Styles", ev)
+  const map = ev.target
+  if (!map.getLayer("custom_buildings")) {
+    map.addLayer(
+      new Layer({ id: "custom_buildings", renderingMode: "3d" })
+    )
+  }
 }
